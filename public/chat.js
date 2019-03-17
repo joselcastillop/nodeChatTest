271a2c45b507d7eeafd58014ba57eee42b39e4af
Jose Castillo <jose.castillo@donuts.ne.jp>
2019-03-17 20:04:51 +0900

    indent again

--- public/chat.js
+++ public/chat.js
@@ -1,14 +1,14 @@
 $(function(){
-   	//make connection
-	var socket = io.connect('http://localhost:3000')
+    //make connection
+    var socket = io.connect('http://localhost:3000')
 
-	//buttons and inputs
-	var message = $("#message")
-	var username = $("#username")
-	var send_message = $("#send_message")
-	var send_username = $("#send_username")
-	var chatroom = $("#chatroom")
-	var feedback = $("#feedback")
+    //buttons and inputs
+    var message = $("#message")
+    var username = $("#username")
+    var send_message = $("#send_message")
+    var send_username = $("#send_username")
+    var chatroom = $("#chatroom")
+    var feedback = $("#feedback")
 
     //Sends Messages through the socket
     send_message.click(function(){
@@ -48,25 +48,25 @@ $(function(){
         }
     })
 
-	//Listen on new_message
-	socket.on("new_message", (data) => {
-		feedback.html('');
-		message.val('');
-		chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>")
-	})
+    //Listen on new_message
+    socket.on("new_message", (data) => {
+        feedback.html('');
+        message.val('');
+        chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>")
+    })
 
-	//Emit a username
-	send_username.click(function(){
-		socket.emit('change_username', {username : username.val()})
-	})
+    //Emit a username
+    send_username.click(function(){
+        socket.emit('change_username', {username : username.val()})
+    })
 
-	//Emit typing
-	message.bind("keypress", () => {
-		socket.emit('typing')
-	})
+    //Emit typing
+    message.bind("keypress", () => {
+        socket.emit('typing')
+    })
 
-	//Listen on typing
-	socket.on('typing', (data) => {
-		feedback.html("<p><i>" + data.username + " is typing a message..." + "</i></p>")
-	})
+    //Listen on typing
+    socket.on('typing', (data) => {
+        feedback.html("<p><i>" + data.username + " is typing a message..." + "</i></p>")
+    })
 });
